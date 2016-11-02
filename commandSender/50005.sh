02df28862c1d8efe4c6b5597ba326192297fc542
MountainShan <wongtingshan111@gmail.com>
2016-11-03 05:54:48 +0800

    deleted:    commandSender/50000.sh 	deleted:    commandSender/50001.sh 	deleted:    commandSender/50002.sh 	deleted:    commandSender/50003.sh 	deleted:    commandSender/50004.sh 	deleted:    commandSender/50005.sh 	deleted:    commandSenderGenerator.sh 	deleted:    p4Cli.py~ 	modified:   README.md

--- commandSender/50005.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
-source $THIS_DIR/../../../env.sh;
-CLI_PATH=$BMV2_PATH/targets/simple_switch/sswitch_CLI;
-
-$CLI_PATH P4Output.json 50005
